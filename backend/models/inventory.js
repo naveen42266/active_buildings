39ece46f6bc0e149f0ed87b9783c296ed09efc13
Naveen <vnaveenlgp2001@gmail.com>
2024-12-27 09:02:55 +0530

    second commit

--- backend/models/inventory.js
+++ backend/models/inventory.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const InventorySchema = new mongoose.Schema({
-  itemCode: { type: Number, required: true },
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  vendorName: { type: String, required: true },
-  date: { type: Date, required: true },
-});
+// const InventorySchema = new mongoose.Schema({
+//   itemCode: { type: Number, required: true },
+//   name: { type: String, required: true },
+//   price: { type: Number, required: true },
+//   vendorName: { type: String, required: true },
+//   date: { type: Date, required: true },
+// });
 
-const Inventory = mongoose.model("inventory", InventorySchema);
+// const Inventory = mongoose.model("inventory", InventorySchema);
 
-module.exports = Inventory;
+// module.exports = Inventory;
